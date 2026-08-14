daf172f09f09fc4760f64c5aa916a57afbd0ef8e
chickenyzh <2783446697@qq.com>
2026-08-14 09:21:47 +0800

    Add zombie_evolution: src/main/java/com/example/zombieevolution/TimeSpeedManager.java

---
+++ src/main/java/com/example/zombieevolution/TimeSpeedManager.java
@@ -0,0 +1,13 @@
+package com.example.zombieevolution;
+
+public class TimeSpeedManager {
+    private static int timeSpeed = 1;
+
+    public static int getTimeSpeed() {
+        return timeSpeed;
+    }
+
+    public static void setTimeSpeed(int speed) {
+        timeSpeed = speed;
+    }
+}
